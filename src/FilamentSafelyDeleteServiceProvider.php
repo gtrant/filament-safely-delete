83dff742c0de3220a91731b549eb997bcc9a7f5a
Garrett Trant <gtrant@users.noreply.github.com>
2024-05-28 23:20:01 +0100

    Update FilamentSafelyDeleteServiceProvider.php
    Fix for Class "Filament\PluginServiceProvider" not found

--- src/FilamentSafelyDeleteServiceProvider.php
+++ src/FilamentSafelyDeleteServiceProvider.php
@@ -4,8 +4,10 @@ namespace Konnco\FilamentSafelyDelete;
 
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Filament\Support\Facades\FilamentView;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentSafelyDeleteServiceProvider extends PluginServiceProvider
+class FilamentSafelyDeleteServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
